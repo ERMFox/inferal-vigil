9d0fadd4a0ab9c9da655141453b36bc894663b22
ERMFox <lonewaygaming@gmail.com>
2024-06-14 09:52:21 +0200

    restructured paths and added main menu scene

--- Scripts/Worldloader.gd
+++ Scripts/Worldloader.gd
@@ -9,13 +9,13 @@ var difficulty
 func _ready():
 	# initiating the player
 	Player = $Player
-	var player_character_path = self.player_data if self.player_data else "res://PlayerCharacters/hero.tscn"
+	var player_character_path = self.player_data if self.player_data else "res://hero.tscn"
 	var player_character = load(player_character_path)
 	Player.add_child(player_character, "PlayerCharacter")
 	# initiating the scene
-	var scene_path = self.scene_data if self.scene_data else "res://Worlds/grasslands.tcsn"
+	var scene_path = self.scene_data if self.scene_data else "res://grasslands"
 	var loaded_scene = load(scene_path)
-	self.add_child(loaded_scene)
+	self.add_child(loaded_scene, "World")
 	# initiating world settings
 	World = $World
 	EnemyTypes = World.getEnemyTypes()
