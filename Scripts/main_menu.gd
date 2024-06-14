c4c0ca4e9b807c482a210be52c0f0baf87e5ddce
ERMFox <lonewaygaming@gmail.com>
2024-06-14 10:26:31 +0200

    work on main menu

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_quit_button_down():
+	var confirmation = $"Confirmation Dialogue"
+	confirmation.show()
+	pass # Replace with function body.
+
+
+func _on_confirmation_dialogue_id_pressed(id):
+	if id == 1:
+		get_tree().quit()
+		
